b41de18881980afa79e7196cb9cefb026a3e5374
André Luiz Mendonça <andreluzz@gmail.com>
2018-04-11 20:13:01 -0300

    teste

--- main.go
+++ main.go
@@ -5,7 +5,10 @@ import (
 	"github.com/andreluzz/cd-test/parser"
 )
 
+var version = "Development"
+
 func main() {
 	parsedString := parser.Translate("Testing continuous delivery features.")
 	fmt.Println(parsedString)
+	fmt.Println("Version: " + version)
 }
